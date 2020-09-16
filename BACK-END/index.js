728c09692f8a836355d17f831d43c65c69185298
Stella Vilar <vilar.stella@gmail.com>
2020-09-16 17:54:31 +0200

    Server initialized with Express

---
+++ BACK-END/index.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const app = express();
+
+const PORT = process.env.PORT || 1234;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
