4cba6a78cfc660c1add2b14b53b7e5a95ed34559
Stella Vilar <vilar.stella@gmail.com>
2020-09-21 16:05:38 +0200

    Favicon installed and body scss, fonts are ok

---
+++ FRONT-END/funny-quiz/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world this is stella
+      
+    </div>
+  );
+}
+
+export default App;
